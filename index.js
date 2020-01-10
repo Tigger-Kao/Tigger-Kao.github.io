64b9786db7e216f3f5477c28084218f608e27ca0
Tigger <tea411139@hotmail.com>
2020-01-10 12:58:54 +0800

    0110

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
         nav.classList.toggle('show');
     });
 
-    $('nav .row a').click(function(){
+    $('nav .row a').click(function () {
         nav.classList.toggle('show');
     })
 
@@ -113,15 +113,22 @@
         $slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
             var calc = ((nextSlide) / (slick.slideCount - 1)) * 100;
             // $progressBar
-                // .css('background-size', calc + '% 100%')
-                // .attr('aria-valuenow', calc);
+            // .css('background-size', calc + '% 100%')
+            // .attr('aria-valuenow', calc);
             $progressBarLabel.text(calc + '% completed');
         });
 
         $slider.slick({
             slidesToShow: 3,
             slidesToScroll: 1,
-            speed: 400
+            speed: 400,
+            responsive: [{
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                }
+            ]
         });
     });
 
